ed7cad03d40477b3a7ff3916b30792b0202d1df5
Vlasenko1710 <112345220+Vlasenko1710@users.noreply.github.com>
2023-07-17 19:45:40 +0300

    Update schemas.js

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -5,7 +5,12 @@ const addSchema = Joi.object({
   email: Joi.string().required(),
   phone: Joi.string().required(),
 });
-
+const updSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
 module.exports = {
   addSchema,
+  updSchema
 };
