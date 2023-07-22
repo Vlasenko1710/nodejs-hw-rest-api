9ab35fb6b2ec9c64e5a124c06b8b57dffa89a424
Vlasenko1710 <vlasenko_ea@ukr.net>
2023-07-23 00:03:13 +0300

    add_hw04

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,19 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authorization } = require("../../middlewares");
 const { schemas } = require("../../models/contacts");
 
-router.get("/", ctrl.getAll);
+router.get("/", authorization, ctrl.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authorization, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authorization, validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", isValidId, ctrl.deleteById);
+router.delete("/:contactId", authorization, isValidId, ctrl.deleteById);
 
 router.put(
   "/:contactId",
+  authorization,
   isValidId,
   validateBody(schemas.updSchema),
   ctrl.updateById
@@ -23,6 +24,7 @@ router.put(
 
 router.patch(
   "/:contactId/favorite",
+  authorization,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
